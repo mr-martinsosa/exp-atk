45681b91fa91d97ec973a3a619f27baa39b0fc86
Md adnanul Chowdhury <adnanul1993@gmail.com>
2018-05-25 16:35:58 -0400

    added content to all the controller, edited routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-
+  root to: 'home#index'
+  resources :comments
+  resources :games
+  resources :posts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
