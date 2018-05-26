351aa80c2e447d16ad614770e715d7c8e2173fcc
Md adnanul Chowdhury <adnanul1993@gmail.com>
2018-05-26 16:42:31 -0400

    added index, show, edit, destroy, update, create, and new methods into all the controller files. And also added relevent views files.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,22 @@
 class CommentsController < ApplicationController
-    def index
-    end
-    
-    def show
-    end
+  def index
+  end
 
-    def edit
-    end
+  def show
+  end
 
-    def destroy
-    end
+  def edit
+  end
 
-    def update
-    end
+  def destroy
+  end
 
-    def create
-    end
+  def update
+  end
 
-    def new
-    end
+  def create
+  end
+
+  def new
+  end
 end
