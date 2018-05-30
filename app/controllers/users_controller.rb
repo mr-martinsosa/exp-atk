360d66be23cb4964bfad78029309208c2d108dc5
Md adnanul Chowdhury <adnanul1993@gmail.com>
2018-05-30 14:46:20 -0400

    trying to workout users_controller to present information on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,4 +33,10 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :username, :password, :email, :birthday)
+    end
 end
