2730c1c194d2fc34b87f17da79739f5b70f613eb
Rachel Bressner <rachel.bressner@gmail.com>
2018-06-03 13:50:00 -0400

    removed password from users table since it interfered with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_03_171514) do
+ActiveRecord::Schema.define(version: 2018_06_03_174810) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 2018_06_03_171514) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "password"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
