ff63335c6c63ed047d77fbd76cde05a831779c72
Rachel Bressner <rachel.bressner@gmail.com>
2018-06-03 13:16:51 -0400

    DEVISE WORKS

---
+++ db/migrate/20180603165249_create_users_table.rb
@@ -0,0 +1,10 @@
+class CreateUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.date :birthday
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_183314) do
+ActiveRecord::Schema.define(version: 2018_06_03_171514) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -39,12 +39,11 @@ ActiveRecord::Schema.define(version: 2018_06_01_183314) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
+    t.string "first_name"
+    t.string "last_name"
     t.string "username"
     t.date "birthday"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -54,8 +53,8 @@ ActiveRecord::Schema.define(version: 2018_06_01_183314) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "password"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
